cbf29cf8ca86691b02c742cbd33574982a2f9d99
Heng Li <lh3@live.co.uk>
2011-03-28 15:27:35 -0400

    add tv count

--- utils/cntcpg.c
+++ utils/cntcpg.c
@@ -31,24 +31,6 @@ char *seq_nt16_rev_table = "XACMGRSVTWYHKDBN";
 uint8_t seq_nt16_nt4_table[] = { 4, 0, 1, 4, 2, 4, 4, 4, 3, 4, 4, 4, 4, 4, 4, 4 };
 int bitcnt_table[] = { 4, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
 
-void cnt_het2(const char *seq, const uint8_t *s, int l, int x[4])
-{
-	int i, cnt[4];
-	cnt[0] = cnt[1] = cnt[2] = cnt[3] = 0;
-	for (i = 0; i < l; ++i) {
-		int c0 = bitcnt_table[seq_nt16_table[(int)seq[i]]];
-		int c1 = bitcnt_table[s[i]];
-		if (c0 < 3) ++cnt[0];
-		if (c0 == 2) ++cnt[1];
-		if (c1 < 3) ++cnt[2];
-		if (c1 == 2) ++cnt[3];
-	}
-	x[0] = cnt[0] - cnt[2];
-	x[1] = cnt[1] - cnt[3];
-	x[2] = cnt[0] - x[0];
-	x[3] = cnt[1] - x[1];
-}
-
 int main(int argc, char *argv[])
 {
 	gzFile fp;
@@ -68,13 +50,13 @@ int main(int argc, char *argv[])
 	}
 	fp = strcmp(argv[optind], "-")? gzopen(argv[optind], "r") : gzdopen(fileno(stdin), "r");
 	seq = kseq_init(fp);
-	l = 4; // number of counts
+	l = 5; // number of counts
 	fwrite(&l, 4, 1, stdout);
 	while ((l = kseq_read(seq)) >= 0) {
-		int32_t i, z[4];
+		int32_t i, z[5];
 		uint8_t *s;
 		s = calloc(l, 1);
-		for (i = 0; i < l; ++i) { // create a copy
+		for (i = 0; i < l; ++i) { // mask lowcase bases and create a copy
 			if (islower(seq->seq.s[i])) seq->seq.s[i] = 'N';
 			seq->seq.s[i] = s[i] = seq_nt16_table[(int)seq->seq.s[i]];
 		}
@@ -85,26 +67,28 @@ int main(int argc, char *argv[])
 		}
 		i = (l + step - 1) / step;
 		fwrite(&i, 4, 1, stdout); // write length
-		z[0] = z[1] = z[2] = z[3] = 0;
+		z[0] = z[1] = z[2] = z[3] = z[4] = 0;
 		for (i = 0; i < l; ++i) {
-			int c0 = bitcnt_table[(int)seq->seq.s[i]];
+			int b = seq->seq.s[i];
+			int c0 = bitcnt_table[b];
 			int c1 = bitcnt_table[s[i]];
 			if (i && i%step == 0) {
-				if (step - z[0] > step * n_ratio) z[0] = z[1] = z[2] = z[3] = 0;
+				if (step - z[0] > step * n_ratio) z[0] = z[1] = z[2] = z[3] = z[4] = 0;
 				z[0] -= z[2]; z[1] -= z[3];
-				if (bin) z[1] = (z[1] >= 1), z[3] = (z[3] >= 1);
-				fwrite(z, 4, 4, stdout);
-				z[0] = z[1] = z[2] = z[3] = 0;
+				if (bin) z[1] = (z[1] >= 1), z[3] = (z[3] >= 1), z[4] = (z[4] >= 1);
+				fwrite(z, 4, 5, stdout);
+				z[0] = z[1] = z[2] = z[3] = z[4] = 0;
 			}
 			if (c0 < 3) ++z[0];
 			if (c0 == 2) ++z[1];
 			if (c1 < 3) ++z[2];
 			if (c1 == 2) ++z[3];
+			if (c0 == 2 && b != 5 && b != 10) ++z[4]; // transversions
 		}
-		if (step - z[0] > step * n_ratio) z[0] = z[1] = z[2] = z[3] = 0;
+		if (step - z[0] > step * n_ratio) z[0] = z[1] = z[2] = z[3] = z[4] = 0;
 		z[0] -= z[2]; z[1] -= z[3];
-		if (bin) z[1] = (z[1] >= 1), z[3] = (z[3] >= 1);
-		fwrite(z, 4, 4, stdout);
+		if (bin) z[1] = (z[1] >= 1), z[3] = (z[3] >= 1), z[4] = (z[4] >= 1);
+		fwrite(z, 4, 5, stdout);
 		free(s);
 	}
 	kseq_destroy(seq);
