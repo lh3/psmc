16081af4bab3f0e750b9ec7c4d3079045fc5bdd7
Heng Li <lh3@me.com>
2012-11-17 23:43:39 -0500

    compute average population size

---
+++ utils/avg.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use Getopt::Std;
+
+my %opts = (n=>20, s=>100, b=>0, e=>999);
+getopts('n:s:b:e:', \%opts);
+
+my $flag = 0;
+my ($theta, @t, @s, $MT);
+while (<>) {
+	$flag = 1 if (/^RD\t(\d+)/ && $1 == $opts{n});
+	if ($flag) {
+		$theta = $1 / $opts{s} if (/^TR\t(\S+)/);
+		$MT = $1 if /^MT\t(\S+)/;
+		if (/^RS\t(\S+)\t(\S+)\t(\S+)/) {
+			$t[$1] = $2 * $theta; $s[$1] = $3 * $theta;
+		}
+		last if (/^\/\//);
+	}
+}
+push(@t, 1000);
+#for my $k (0 .. $#s) { print("$t[$k]\t$t[$k+1]\t$s[$k]\n"); }
+
+my $n = @s;
+my @alpha = (1);
+for my $k (1 .. $n) {
+	$alpha[$k] = $alpha[$k-1] * exp(-($t[$k] - $t[$k-1]) / $s[$k-1]);
+}
+
+my ($bk, $ek);
+for my $k (1 .. $n) {
+	$bk = $k - 1 if ($opts{b} < $t[$k] && $opts{b} >= $t[$k-1]);
+	$ek = $k - 1 if ($opts{e} < $t[$k] && $opts{e} >= $t[$k-1]);
+}
+
+my $x = $alpha[$bk] * exp(-($opts{b} - $t[$bk]) / $s[$bk]) - $alpha[$ek] * exp(-($opts{e} - $t[$ek]) / $s[$ek]);
+
+my $a1 = 0;
+for (my $i = 0; $i < $bk; ++$i) {
+	$a1 += $s[$i] * ($alpha[$i] - $alpha[$i+1]);
+}
+$a1 += $s[$bk] * $alpha[$bk] * (1 - exp(-($opts{b} - $t[$bk]) / $s[$bk]));
+
+my $a2 = 0;
+for (my $i = 0; $i < $ek; ++$i) {
+	$a2 += $s[$i] * ($alpha[$i] - $alpha[$i+1]);
+}
+$a2 += $s[$ek] * $alpha[$ek] * (1 - exp(-($opts{e} - $t[$ek]) / $s[$ek]));
+
+my $avg = ($a2 - $a1) / $x;
+
+print "$avg\n";
